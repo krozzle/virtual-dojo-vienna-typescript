9b072b5aa213b2e6905d59ad24aae6a512593592
Christopher Wattaul <chris.wattaul@gmail.com>
2020-07-24 10:43:56 +0200

    Add files for heroku deploy

---
+++ db.js
@@ -0,0 +1,12 @@
+require('./extractHerokuDatabaseEnvVars')();
+require('dotenv').config();
+
+const postgres = require('postgres');
+
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
