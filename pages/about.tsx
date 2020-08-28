5762bddadce66573ccf33caf50624f80d8210b83
Christopher Wattaul <chris.wattaul@gmail.com>
2020-08-28 15:39:42 +0200

    Add styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,25 +1,18 @@
-import { Text, Flex } from 'theme-ui';
 import Page from '../components/Page';
 
 const AboutPage = () => (
   <Page>
-    <Flex
-      sx={{
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <Text variant='pageText' as='p'>
-        <h1>Who We Be</h1>
+    <div class='flex max-w-full  min-h-full justify-around my-2 p-5'>
+      <div class='text-white w-2/3 text-xl p-2 text-left bg-white bg-opacity-25 border border-black'>
         As the home of Austria's fighting game community, we provide a place to
-        meet and get together with fighting game enthusiasts.
-      </Text>
-      <Text variant='pageText' as='p'>
-        Engage with people that share the same passion casually or participate
-        in our local ranking tournaments.
-      </Text>
-    </Flex>
+        meet and get together with fighting game enthusiasts. Engage with people
+        that share the same passion casually or participate in our local ranking
+        tournaments.
+      </div>
+      <div class='text-white text-5xl w-1/3 font-bold p-4 text-center uppercase p-2  bg-black bg-opacity-50'>
+        who we be
+      </div>
+    </div>
   </Page>
 );
 
--- pages/impressum.js
+++ pages/impressum.js
@@ -6,12 +6,16 @@ import Page from '../components/Page';
 const Impressum = () => {
   return (
     <Page>
-      <div class='flex flex-col min-w-xl justify-around p-5'>
-        <h1 class='text-white text-5xl font-bold p-4 text-center'>
+      <div class='flex flex-col min-h-full justify-around p-5 my-2'>
+        <div class='text-white text-5xl font-bold p-4 text-center'>
           Legal Notice
-        </h1>
-        <div class='text-white font-bold text-left p-2 text-3xl'>Address</div>
-        <div class='text-white py-3 text-left p-2'>
+        </div>
+        <div class='text-white text-4xl font-bold py-2 text-left'>
+          Virtual Dojo Vienna - Verein zur Förderung der Videospielkultur
+        </div>
+        <div class='text-white text-4xl font-bold  text-left' />
+        <div class='text-white font-bold text-left py-2 text-3xl'>Address</div>
+        <div class='text-white py-1 text-left'>
           <a
             href='https://goo.gl/maps/tHa1X6nNoYVDq3Ky5'
             class='hover:underline'
@@ -19,28 +23,20 @@ const Impressum = () => {
             Praterstrasse 58/1/L1 1020 Vienna
           </a>
         </div>
-        <br />
-        <div class='text-white font-bold text-left p-2 text-3xl'>
-          Email
-        </div>{' '}
-        <div class='text-white py-3 text-left p-2 '>
+        <div class='text-white font-bold text-left py-2 text-3xl'>Email</div>{' '}
+        <div class='text-white py-1 text-left  '>
           <a href='mailto:office@virtualdojovienna.com' class='hover:underline'>
             office@virtualdojovienna.com
           </a>
         </div>
-        <br />
-        <div class='text-white font-bold text-left p-2 text-3xl'>
-          Phone
-        </div>{' '}
-        <div class='text-white py-3 text-left p-2'>+43 1 27 67 824</div>
-        <br />
-        <div class='text-white font-bold text-left p-2 text-3xl'>Bank</div>
-        <div class='text-white py-3 text-left p-2'>
+        <div class='text-white font-bold text-left py-2 text-3xl'>Phone</div>{' '}
+        <div class='text-white py-3 text-left py-2'>+43 1 27 67 824</div>
+        <div class='text-white font-bold text-left py-2 text-3xl'>Bank</div>
+        <div class='text-white py-1 text-left '>
           IBAN: AT94 2011 1842 6118 4800
         </div>
-        <div class='text-white py-3 text-left p-2'>BIC: GIBAATWWXXX</div>
+        <div class='text-white py-3 text-left py-2'>BIC: GIBAATWWXXX</div>
       </div>
-      
     </Page>
   );
 };
