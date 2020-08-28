100f86c59fa8a8922c777bdc9f27b9e4fe639556
Christopher Wattaul <chris.wattaul@gmail.com>
2020-08-28 16:21:40 +0200

    Add styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,15 +2,16 @@ import Page from '../components/Page';
 
 const AboutPage = () => (
   <Page>
-    <div class='flex max-w-full  min-h-full justify-around my-2 p-5'>
-      <div class='text-white w-2/3 text-xl p-2 text-left bg-white bg-opacity-25 border border-black'>
+    <div class='flex flex-col w-full bg-red-700 min-h-full justify-around my-2 p-5'>
+      <div class='text-white text-left px-2 py-1'>about us</div>
+      <div class='text-black text-left text-4xl font-bold px-2 py-1 uppercase border-b  border-white'>
+        who we be
+      </div>
+      <div class='text-white w-2/3 text-xl p-2 text-left  '>
         As the home of Austria's fighting game community, we provide a place to
         meet and get together with fighting game enthusiasts. Engage with people
         that share the same passion casually or participate in our local ranking
         tournaments.
-      </div>
-      <div class='text-white text-5xl w-1/3 font-bold p-4 text-center uppercase p-2  bg-black bg-opacity-50'>
-        who we be
       </div>
     </div>
   </Page>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,16 @@ const IndexPage = () => (
     <div class='flex min-h-full justify-around p-2 my-2'>
       <div>
         <div class='text-white text-3xl bg-white bg-opacity-25 font-bold p-2 text-center uppercase'>
-          Home of the Austrian Fighting Game Community.{' '}
-        </div>
-        <div class='text-white text-3xl bg-white bg-opacity-25 font-bold p-2 text-center uppercase'>
-          Since 2010.{' '}
+          Home of the Austrian Fighting Game Community{' '}
         </div>
       </div>
       <div class='flex flex-col  justify-around text-white bg-black bg-opacity-75 p-2 text-center text-2xl '>
         <div class=''>
           Enabling people of all backgrounds to get together and enjoy Fighting
-          Games the way they are meant to be played.
+          Games the way they are meant to be played
         </div>
         <div class='text-white bg-black bg-opacity-75 p-2 text-center text-2xl uppercase'>
-          Competitively.
+          Competitively
         </div>
       </div>
     </div>
