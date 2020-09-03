d35dea677e085b72e0760ef9789ace6ebf6d3fd5
Christopher Wattaul <chris.wattaul@gmail.com>
2020-09-03 14:57:49 +0200

    Add styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,22 @@ import Page from '../components/Page';
 
 const IndexPage = () => (
   <Page>
-    <div class="flex min-h-full justify-around p-2 my-2">
-      <div>
-        <div class="text-white text-3xl bg-white bg-opacity-25 font-bold p-2 text-center uppercase">
-          Home of the Austrian Fighting Game Community{' '}
-        </div>
+    <div className="flex min-h-full justify-around p-2 my-2">
+      <div className="text-white text-3xl w-1/3 h-full bg-white bg-opacity-25 font-bold p-2 uppercase">
+        Home of the Austrian Fighting Game Community{' '}
       </div>
-      <div class="flex flex-col  justify-around text-white bg-black bg-opacity-75 p-2 text-center text-2xl ">
-        <div class="">
-          Enabling people of all backgrounds to get together and enjoy Fighting
-          Games the way they are meant to be played
-        </div>
-        <div class="text-white bg-black bg-opacity-75 p-2 text-center text-2xl uppercase">
-          Competitively
-        </div>
+      <div className="flex text-white text-5xl h-full bg-black bg-opacity-75 font-bold p-2 uppercase">
+        Virtual Dojo <span className="text-red-600">| </span>{' '}
+        <p className="font-light">Vienna</p>
+      </div>
+    </div>
+    <div className="flex flex-col justify-around text-white bg-black bg-opacity-75 p-2 text-center text-2xl ">
+      <div className="">
+        Enabling people of all backgrounds to get together and enjoy Fighting
+        Games the way they are meant to be played
+      </div>
+      <div className="text-white bg-black bg-opacity-75 p-2 text-center text-2xl uppercase">
+        Competitively
       </div>
     </div>
   </Page>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      backgroundImage: theme => ({
-        'hero-pattern': "url('/header.png')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      }),
+      // backgroundImage: theme => ({
+      //   'hero-pattern': "url('/header.png')",
+      //   'footer-texture': "url('/img/footer-texture.png')",
+      // }),
     },
   },
   variants: {},
