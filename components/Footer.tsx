73282be738bf71f5bc117f86371e908d01ce0e5e
Christopher Wattaul <chris.wattaul@gmail.com>
2020-10-14 22:10:58 +0200

    Add Styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,40 +1,40 @@
 const Footer = () => (
-  <div className="flex justify-around min-h-full bg-black text-white py-2">
+  <div className="flex justify-between min-h-full bg-black bg-opacity-75 text-white py-2 px-32">
     <div className="flex align-middle py-1">
       {/* <p className="text-lg pr-4 py-1">Follow us</p> */}
       <a href="https://www.twitch.tv/virtualdojovienna">
         <img
-          className="px-2 hover:bg-gray-300 hover:bg-opacity-25"
+          className="px-2 hover:bg-black hover:bg-opacity-75"
           src="/twitch.svg"
         />
       </a>
       <a href="https://youtube.com/virtualdojovienna">
         <img
-          className="px-2 hover:bg-gray-300 hover:bg-opacity-25"
+          className="px-2 hover:bg-black hover:bg-opacity-75"
           src="/youtube.svg"
         />
       </a>
       <a href="https://twitter.com/virtualdojo">
         <img
-          className="px-2 hover:bg-gray-300 hover:bg-opacity-25"
+          className="px-2 hover:bg-black hover:bg-opacity-75"
           src="/twitter.svg"
         />
       </a>
       <a href="https://www.instagram.com/virtualdojovienna">
         <img
-          className="px-2 hover:bg-gray-300 hover:bg-opacity-25"
+          className="px-2 hover:bg-black hover:bg-opacity-75"
           src="/instagram.svg"
         />
       </a>
       <a href="https://www.facebook.com/VirtualDojoVienna/">
         <img
-          className="px-2 hover:bg-gray-300 hover:bg-opacity-25"
+          className="px-2 hover:bg-black hover:bg-opacity-75"
           src="/facebook.svg"
         />
       </a>
     </div>
     <div className="flex justify-center align-middle text-lg py-1">
-      © {new Date().getFullYear()} Virtual Dojo Vienna
+      © {new Date().getFullYear()}
     </div>
     <div className="flex justify-center align-middle">
       <a href="/statuten_2016.pdf" download>
